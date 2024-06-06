ce6e1313586f3806c48cf53aba2b4f464ba190e6
sdumbre1 <sdumbre@charlotte.edu>
2024-06-06 20:26:55 +0530

    Made Changes

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express = require('express')
 const app = express();
+const path = require('path')
 const UrlRoute = require('./routes/url')
+const staticRouter = require('./routes/staticrouter')
 const port = 8100
 const {connectMongoDB} = require('./connect')
 const URL = require('./models/url')
 
 //Middleware
 app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 
 //Mongo connection
 connectMongoDB('mongodb://localhost:27017/short-url').then(()=>console.log("MongoDB connected"))
 
+app.set("view engine","ejs")
+app.set("views",path.resolve('./views'))
+
+// app.get('/test',async(req,res)=>{
+//     const allURLS = await URL.find({})
+//     return res.render('list',{
+//         urls: allURLS
+//     })
+// })
 //routes
 app.use('/url',UrlRoute)
 app.get('/:shortid',async (req,res)=>{
@@ -22,5 +34,6 @@ app.get('/:shortid',async (req,res)=>{
     );
     res.redirect(entry.redirectUrl)
 })
+app.use('/',staticRouter)
 
 app.listen(port,()=>console.log(`Server started at port ${port}`))
